e93e4e1a108aa3e22c6d26d4d47ef6c5e20a1f95
H2W0N <woohw97@naver.com>
2022-07-05 17:31:09 +0900

    ch2 2.변수의 타입

---
+++ Chapter02/StringEx.java
@@ -0,0 +1,16 @@
+class StringEx {
+    public static void main(String[] args) {
+        String name = "JA" + "VA";
+        String str = name + 8.0; // 문자열 + 숫자 = 문자열
+
+        System.out.println(name); // JAVA
+        System.out.println(str); // JAVA8.0
+        System.out.println(7 + " "); // 7
+        System.out.println(" " + 7); //  7
+        System.out.println(7 + ""); // 7
+        System.out.println("" + 7); // 7
+        System.out.println("" + ""); // 아무것도 나오지 않음. 공백
+        System.out.println(7 + 7 + ""); // 14
+        System.out.println("" + 7 + 7); // 77
+    }    
+}
